2cb138a83fe45afbfed8c51c7a498e4d40e9c514
msannac <msannac783@g.educaand.es>
2024-05-02 14:37:12 +0200

    Ayuda

---
+++ src/main/java/app/panels/Ayuda.java
@@ -0,0 +1,53 @@
+package app.panels;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Ayuda extends Application {
+
+	 @Override
+	    public void start(Stage primaryStage) {
+	        // Creamos un objeto VBox para organizar los elementos de la interfaz
+	        VBox vbox = new VBox();
+	        vbox.setAlignment(Pos.CENTER);
+	        vbox.setSpacing(10);
+	        vbox.setPadding(new Insets(25, 25, 25, 25));
+
+	        // Agregamos cinco enlaces a la interfaz
+	        Hyperlink link1 = new Hyperlink("Link 1");
+	        Hyperlink link2 = new Hyperlink("Link 2");
+	        Hyperlink link3 = new Hyperlink("Link 3");
+	        Hyperlink link4 = new Hyperlink("Link 4");
+	        Hyperlink link5 = new Hyperlink("Link 5");
+
+	        vbox.getChildren().addAll(link1, link2, link3, link4, link5);
+	        
+	     // Agregamos un enlace a GitHub
+	        Hyperlink githubLink = new Hyperlink("GitHub Repository");
+	        githubLink.setVisited(false);
+	        githubLink.setOnAction(event -> {
+	            getHostServices().showDocument("https://github.com/jjenkov/javafx-examples");
+	        });
+	        vbox.getChildren().add(githubLink);
+
+	        // Agregamos un copyright a la interfaz
+	        Label copyrightLabel = new Label("Copyright © 2023 JavaMusic");
+	        vbox.getChildren().add(copyrightLabel);
+
+	        // Creamos una escena y la agregamos a la ventana
+	        Scene scene = new Scene(vbox, 300, 250);
+	        primaryStage.setScene(scene);
+	        primaryStage.setTitle("Ayuda");
+	        primaryStage.show();
+	    }
+
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
+}
